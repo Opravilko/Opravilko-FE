f9d0ea34f8b575dd043a42cb6e5109641ef00012
zzeo-png <115707223+StudentLeoO@users.noreply.github.com>
2024-03-16 20:35:00 +0100

    updated visuals for messages & chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -92,6 +92,15 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 7,
+        },
+        shadowOpacity: 0.43,
+        shadowRadius: 9.51,
+
+        elevation: 15,
     },
     header: {
         color: "white",
--- screens/MessagesScreen.js
+++ screens/MessagesScreen.js
@@ -118,6 +118,15 @@ const styles = StyleSheet.create({
         borderBottomRightRadius: 30,
         borderBottomLeftRadius: 30,
         justifyContent: "flex-end",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 7,
+        },
+        shadowOpacity: 0.43,
+        shadowRadius: 9.51,
+
+        elevation: 15,
     },
     header: {
         color: "white",
